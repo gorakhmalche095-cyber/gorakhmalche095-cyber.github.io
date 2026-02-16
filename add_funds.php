7095fa026200f0b1241ca89caec11e3461f002b4
gorakhmalche095-cyber <gorakhmalche095@gmail.com>
2026-02-16 08:38:39 +0530

    Add files via upload

---
+++ add_funds.php
@@ -0,0 +1,8 @@
+<form action="phonepe_create_payment.php" method="POST">
+    <h2>Add Funds</h2>
+
+    <label>Enter Amount (₹)</label><br>
+    <input type="number" name="amount" min="10" required><br><br>
+
+    <button type="submit">Pay Now</button>
+</form>
